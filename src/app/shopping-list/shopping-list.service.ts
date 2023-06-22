b89e67bdb1747f72b52951e9073f7c78c95e7327
impl-Aoki <aoki.mt@impl.co.jp>
2023-06-22 18:43:45 +0900

    fix

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.mode";
 
 export class ShoppingListService {
-    ingredients: Ingredient[] = [
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
     ];
@@ -12,5 +14,6 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
